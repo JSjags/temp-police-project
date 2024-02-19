c10129fc64876b5d56905d7ac048ce2d4bf29a57
Agbugba David C <dagbugba@yahoo.com>
2024-02-19 09:37:17 +0100

    added live backend url

--- src/configs/environment.config.js
+++ src/configs/environment.config.js
@@ -1,13 +1,15 @@
 // enviroments
 const environment = {
   production: {
-    API_BASE_URL: "",
+    API_BASE_URL:
+      "https://police-project-api-c0b1388972f6.herokuapp.com/api/v1",
   },
   development: {
     API_BASE_URL: "http://localhost:5000/api/v1/",
   },
 };
 
-const currentEnvironment = import.meta.env.VITE_REACT_APP_ENV || "development";
+// const currentEnvironment = import.meta.env.VITE_REACT_APP_ENV || "development";
+const currentEnvironment = "production";
 
 export default environment[currentEnvironment];
